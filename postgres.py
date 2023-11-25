531abbc32dc329fd1f1e39b3f1a0a600e20216b0
tau <36781951+tndd@users.noreply.github.com>
2023-11-26 02:41:03 +0900

    version of sql_alchemy.
    time:  3.9390361309051514s

--- postgres.py
+++ postgres.py
@@ -1,4 +1,8 @@
-import psycopg2
+import json
+import time
+
+from sqlalchemy import create_engine
+from sqlalchemy.sql import text
 
 # PostgreSQL database details
 database = "stock_data"
@@ -7,27 +11,59 @@ password = "postgres"
 host = "localhost"
 port = "5432"
 
-# Connect to the PostgreSQL database
-connection = psycopg2.connect(
-    database=database,
-    user=user,
-    password=password,
-    host=host,
-    port=port
-)
+# Create engine
+engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{database}')
 
-# Create a cursor object
-cursor = connection.cursor()
+# assets.jsonからデータを読み込む
+with open('assets.json') as f:
+    data = json.load(f)
 
-# データベースの一覧を取得するクエリを実行
-cursor.execute("SELECT datname FROM pg_database")
+# テーブルを作成
+with engine.connect() as connection:
+    connection.execute(text("""
+        CREATE TABLE IF NOT EXISTS assets (
+            id TEXT PRIMARY KEY,
+            asset_class TEXT,
+            exchange TEXT,
+            symbol TEXT,
+            name TEXT,
+            status TEXT,
+            tradable BOOLEAN,
+            marginable BOOLEAN,
+            shortable BOOLEAN,
+            easy_to_borrow BOOLEAN,
+            fractionable BOOLEAN,
+            min_order_size REAL,
+            min_trade_increment REAL,
+            price_increment REAL
+        )
+    """))
 
-# Fetch all the rows
-rows = cursor.fetchall()
+    # データを削除
+    connection.execute(text("""
+        DELETE FROM assets
+    """))
 
-for row in rows:
-    print(row)
-
-# Close the cursor and connection
-cursor.close()
-connection.close()
+    # データを挿入
+    start_time = time.time()
+    for asset in data:
+        connection.execute(text("""
+            INSERT INTO assets VALUES (
+                :id,
+                :asset_class,
+                :exchange,
+                :symbol,
+                :name,
+                :status,
+                :tradable,
+                :marginable,
+                :shortable,
+                :easy_to_borrow,
+                :fractionable,
+                :min_order_size,
+                :min_trade_increment,
+                :price_increment
+            )
+        """), asset)
+    end_time = time.time()
+    print(f"この一連の処理にかかった時間: {end_time - start_time} 秒")
