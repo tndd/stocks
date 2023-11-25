2c68c99944628e5c611f6158506ecdf29a748570
tau <36781951+tndd@users.noreply.github.com>
2023-11-26 01:21:38 +0900

    test get api assets

---
+++ get_assets.py
@@ -0,0 +1,29 @@
+import os
+
+import pandas as pd
+from alpaca.trading.client import TradingClient
+from alpaca.trading.enums import AssetClass
+from alpaca.trading.requests import GetAssetsRequest
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+secret_key = os.getenv('SECRET_KEY')
+
+
+trading_client = TradingClient(api_key, secret_key)
+
+
+search_params = GetAssetsRequest(asset_class=AssetClass.US_EQUITY)
+assets = trading_client.get_all_assets(search_params)
+
+import time
+import json
+
+start_time = time.time()
+assets_json = [json.loads(asset.model_dump_json()) for asset in assets]
+with open('assets.json', 'w', encoding='utf-8') as f:
+    json.dump(assets_json, f, ensure_ascii=False, indent=4)
+end_time = time.time()
+print(f"この一連の処理にかかった時間: {end_time - start_time} 秒")
+
