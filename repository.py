b9e9f5c944988b55409c4d5a175cf112f4dd10ff
tau <36781951+tndd@users.noreply.github.com>
2023-11-26 20:48:58 +0900

    add repository

---
+++ repository.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import List
+
+from client import AlpacaApiClient, PostgresClient
+from models import Asset
+
+
+@dataclass
+class AssetRepository:
+    psql_client: PostgresClient
+    trading_client: AlpacaApiClient
+
+    def store_assets(self, asset: Asset, data: List[dict]):
+        self.psql_client.insert_models(Asset, data)
+
+    def fetch_store_assets_stock(self):
+        fetched_data = self.trading_client.fetch_assets_stock()
+        self.store_assets(Asset, fetched_data)
+
+    def fetch_store_assets_crypto(self):
+        fetched_data = self.trading_client.fetch_assets_crypto()
+        self.store_assets(Asset, fetched_data)
+
+
+if __name__ == '__main__':
+    import os
+
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    api_key = os.getenv('API_KEY')
+    secret_key = os.getenv('SECRET_KEY')
+
+    psql_client = PostgresClient()
+    alpaca_api_client = AlpacaApiClient(api_key, secret_key)
+
+    asset_rp = AssetRepository(psql_client, alpaca_api_client)
+    asset_rp.fetch_store_assets_stock()
