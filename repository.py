5e0fafc0d79f63f35dc9161200b74062a8d82aff
tau <36781951+tndd@users.noreply.github.com>
2023-11-27 00:34:28 +0900

    implement fetch_store_assets_all()

--- repository.py
+++ repository.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import List
 
 from client import AlpacaApiClient, PostgresClient
 from models import Asset
@@ -10,16 +9,20 @@ class AssetRepository:
     psql_client: PostgresClient
     trading_client: AlpacaApiClient
 
-    def store_assets(self, asset: Asset, data: List[dict]):
+    def store_assets(self, data):
         self.psql_client.insert_models(Asset, data)
 
     def fetch_store_assets_stock(self):
         fetched_data = self.trading_client.fetch_assets_stock()
-        self.store_assets(Asset, fetched_data)
+        self.store_assets(fetched_data)
 
     def fetch_store_assets_crypto(self):
         fetched_data = self.trading_client.fetch_assets_crypto()
-        self.store_assets(Asset, fetched_data)
+        self.store_assets(fetched_data)
+
+    def fetch_store_assets_all(self):
+        self.fetch_store_assets_stock()
+        self.fetch_store_assets_crypto()
 
 
 if __name__ == '__main__':
@@ -36,4 +39,4 @@ if __name__ == '__main__':
     alpaca_api_client = AlpacaApiClient(api_key, secret_key)
 
     asset_rp = AssetRepository(psql_client, alpaca_api_client)
-    asset_rp.fetch_store_assets_stock()
+    asset_rp.fetch_store_assets_all()
