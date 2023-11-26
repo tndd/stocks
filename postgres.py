89d9fad784ccbec4bd9bc57f6daf927e8340939e
tau <36781951+tndd@users.noreply.github.com>
2023-11-26 14:58:09 +0900

    fix scheme

--- postgres.py
+++ postgres.py
@@ -1,12 +1,11 @@
 import json
 import time
 
-import pandas as pd
 from sqlalchemy import Boolean, Column, Float, String, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 # PostgreSQL database details
-database = "stock_data"
+database = "stocks"
 user = "postgres"
 password = "postgres"
 host = "localhost"
@@ -39,27 +38,20 @@ class Asset(Base):
     min_order_size = Column(Float)
     min_trade_increment = Column(Float)
     price_increment = Column(Float)
-    maintenance_margin_requirement = Column(Float)
-    attributes = Column(String)
 
 # assets.jsonからデータを読み込む
 with open('assets.json') as f:
     data = json.load(f)
-
-# データをPandas DataFrameに変換
-df = pd.DataFrame(data)
 
 # テーブルを作成
 Base.metadata.create_all(engine)
 
 # データを削除
 session.query(Asset).delete()
-session.commit()
 
 # データを挿入
 start_time = time.time()
-df.to_sql(Asset.__tablename__, engine, if_exists='append', index=False, method='multi')
+session.bulk_insert_mappings(Asset, data)
+session.commit()
 end_time = time.time()
-
 print(f"この一連の処理にかかった時間: {end_time - start_time} 秒")
-
