452836baddf00259a9904819c4a18555dbe420b3
tau <36781951+tndd@users.noreply.github.com>
2023-11-28 01:21:51 +0900

    separate AssetService

--- repository.py
+++ repository.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import List
 
+from sqlalchemy.orm import Session
+
 from client import AlpacaApiClient, PostgresClient
 from decorators import count_time
 from models import Asset
@@ -13,36 +15,31 @@ class AssetRepository:
     trading_client: AlpacaApiClient
 
     @count_time
-    def store_assets(self, data: List[dict], version: datetime):
+    def stage_assets(self, data: List[dict], version: datetime, session: Session):
         data_with_version = [{'version': version, **asset} for asset in data]
         self.psql_client.insert_models(Asset, data_with_version)
 
-    def fetch_store_assets_stock(self, version: datetime):
-        fetched_data = self.trading_client.fetch_assets_stock()
-        self.store_assets(fetched_data, version)
 
-    def fetch_store_assets_crypto(self, version: datetime):
+@dataclass
+class AssetService:
+    asset_repositoy: AssetRepository
+
+    def fetch_stage_assets_stock(self, version: datetime, session: Session):
+        fetched_data = self.trading_client.fetch_assets_stock()
+        self.asset_repositoy.stage_assets(fetched_data, version, session)
+
+    def fetch_stage_assets_crypto(self, version: datetime, session: Session):
         fetched_data = self.trading_client.fetch_assets_crypto()
-        self.store_assets(fetched_data, version)
+        self.asset_repositoy.stage_assets(fetched_data, version, session)
 
     def update_assets(self):
         version: datetime = datetime.now()
-        self.fetch_store_assets_stock(version)
-        self.fetch_store_assets_crypto(version)
-
-
-if __name__ == '__main__':
-    import os
-
-    from dotenv import load_dotenv
-
-    load_dotenv()
-
-    api_key = os.getenv('API_KEY')
-    secret_key = os.getenv('SECRET_KEY')
-
-    psql_client = PostgresClient()
-    alpaca_api_client = AlpacaApiClient(api_key, secret_key)
-
-    asset_rp = AssetRepository(psql_client, alpaca_api_client)
-    asset_rp.update_assets()
+        session: Session = self.psql_client.create_session()
+        try:
+            session.begin()
+            self.fetch_stage_assets_stock(version, session)
+            self.fetch_stage_assets_crypto(version, session)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise e
