34c795e0d6a3b43464ac8224694117c1f5804a71
tau <36781951+tndd@users.noreply.github.com>
2023-11-28 03:08:52 +0900

    separate file service

--- repository.py
+++ repository.py
@@ -18,28 +18,3 @@ class AssetRepository:
     def stage_assets(self, data: List[dict], version: datetime, session: Session):
         data_with_version = [{'version': version, **asset} for asset in data]
         self.psql_client.schedule_parallel_insert_models(Asset, data_with_version, session)
-
-
-@dataclass
-class AssetService:
-    asset_repositoy: AssetRepository
-
-    def fetch_stage_assets_stock(self, version: datetime, session: Session):
-        fetched_data = self.trading_client.fetch_assets_stock()
-        self.asset_repositoy.stage_assets(fetched_data, version, session)
-
-    def fetch_stage_assets_crypto(self, version: datetime, session: Session):
-        fetched_data = self.trading_client.fetch_assets_crypto()
-        self.asset_repositoy.stage_assets(fetched_data, version, session)
-
-    def update_assets(self):
-        version: datetime = datetime.now()
-        session: Session = self.psql_client.create_session()
-        try:
-            session.begin()
-            self.fetch_stage_assets_stock(version, session)
-            self.fetch_stage_assets_crypto(version, session)
-            session.commit()
-        except Exception as e:
-            session.rollback()
-            raise e
---
+++ service.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from sqlalchemy.orm import Session
+
+from repository import AssetRepository
+
+
+@dataclass
+class AssetService:
+    asset_repositoy: AssetRepository
+
+    def fetch_stage_assets_stock(self, version: datetime, session: Session):
+        fetched_data = self.trading_client.fetch_assets_stock()
+        self.asset_repositoy.stage_assets(fetched_data, version, session)
+
+    def fetch_stage_assets_crypto(self, version: datetime, session: Session):
+        fetched_data = self.trading_client.fetch_assets_crypto()
+        self.asset_repositoy.stage_assets(fetched_data, version, session)
+
+    def update_assets(self):
+        version: datetime = datetime.now()
+        session: Session = self.psql_client.create_session()
+        try:
+            session.begin()
+            self.fetch_stage_assets_stock(version, session)
+            self.fetch_stage_assets_crypto(version, session)
+            session.commit()
+        except Exception as e:
+            session.rollback()
+            raise e
