18675e9c153da872118f26a1a6f0464155ff4785
tau <36781951+tndd@users.noreply.github.com>
2023-12-03 00:13:45 +0900

    add test test_assets_with_generated_data

--- test/domain/asset/test_asset_entity.py
+++ test/domain/asset/test_asset_entity.py
@@ -23,34 +23,12 @@ def test_assets_with_real_data():
     for asset in test_data:
         Asset(**asset)
 
-# def test_asset_model_conversion_insufficient_data():
-#     insufficient_data = {
-#         "id": "f1c42f1f-2444-4714-b866-3144d3548ccd",
-#         'version': '2022-01-01T00:00:00'
-#     }
-#     with pytest.raises(TypeError):
-#         Asset(**insufficient_data)
-
-# def test_asset_model_conversion_excessive_data():
-#     excessive_data = {
-#         'id': '1',
-#         'version': 'v1',
-#         'asset_class': 'class1',
-#         'exchange': 'exchange1',
-#         'symbol': 'symbol1',
-#         'name': 'name1',
-#         'status': 'active',
-#         'tradable': True,
-#         'marginable': True,
-#         'shortable': True,
-#         'easy_to_borrow': True,
-#         'fractionable': True,
-#         'maintenance_margin_requirement': 'requirement1',
-#         'attributes': 'attributes1',
-#         'min_order_size': 1.0,
-#         'min_trade_increment': 0.1,
-#         'price_increment': 0.01,
-#         'extra': 'extra'
-#     }
-#     with pytest.raises(TypeError):
-#         Asset(**excessive_data)
+def test_assets_with_generated_data():
+    """
+    Tested using mock data generated
+        within the range that theoretically conforms to Asset.
+    """
+    mock_name = 'assets_valid_generated'
+    test_data = _load_asset_mock_data(mock_name)
+    for asset in test_data:
+        Asset(**asset)
